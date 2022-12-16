f4d112ccb5134b92202a6295753c9d1d74061964
ademalabas@gmail.com <ademalabas@gmail.com>
2022-12-16 23:05:01 +0300

    16.12.22

---
+++ src/test/java/Day03_Xpath/C02_LocatorPracrice.java
@@ -0,0 +1,62 @@
+package Day03_Xpath;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C02_LocatorPracrice {
+//    Go to https://www.linkedin.com/
+//    When we write an e-mail without the @ sign in the e-mail box and press enter.
+//    Let's test the "Invalid email address" warning.
+
+
+    WebDriver driver;
+
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        //sayfamizin yuklenmesi beklendi
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+
+        // driver imiz maximize edildi
+
+        driver.manage().window().maximize();
+
+
+    }
+
+    @After
+    public void tearDown() {
+
+        // driver imiz kapatildi
+        // driver.quit();
+
+    }
+
+        @Test
+    public void xPathPractice(){
+            //1. adim LinkedIn sitesine gidildi
+            driver.get("https://www.linkedin.com");
+
+           // 2. adim email alanina @ sembolu bulunmayan bir email adresi gir
+            driver.findElement(By.xpath("//input[@id='session_key']"))
+                    .sendKeys("ademalabas"+ Keys.ENTER);
+        // 3. Adim Hata mesejinin goruntulendigini dogrula
+
+
+    }
+
+
+
+
+}
