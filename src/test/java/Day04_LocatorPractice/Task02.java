2807189c7c2bbdb9f9daf80bac83189449a7475c
ademalabas@gmail.com <ademalabas@gmail.com>
2022-12-18 23:12:27 +0300

    18.12.22

---
+++ src/test/java/Day04_LocatorPractice/Task02.java
@@ -0,0 +1,15 @@
+package Day04_LocatorPractice;
+
+public class Task02 {
+//Navigate to https://testpages.herokuapp.com/styled/index.html
+// Click on Calculate under Micro Apps.
+// Type any number in the first input.
+// Type any number in the second input.
+// Click on Calculate.
+// Get the result.
+// Print the result.
+
+
+
+
+}
