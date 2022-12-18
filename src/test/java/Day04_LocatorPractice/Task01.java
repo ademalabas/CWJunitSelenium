7e2ff4a36d63ef51a28c3d25b127ea2bca6355de
ademalabas@gmail.com <ademalabas@gmail.com>
2022-12-18 23:10:57 +0300

    18.12.22

---
+++ src/test/java/Day04_LocatorPractice/Task01.java
@@ -0,0 +1,15 @@
+package Day04_LocatorPractice;
+
+public class Task01 {
+//https://id.heroku.com/login sayfasına gidiniz.
+//Bir mail adresi giriniz.
+//Bir password giriniz.
+//Login butonuna tıklayınız.
+//"There was a problem with your login." text görünür ise "Kayıt Yapılamadı" yazdırınız.
+//Eğer yazı görünür değilse "Kayıt Yapıldı" yazdırınız.
+//Tüm sayfaları kapatınız.
+
+
+
+
+}
