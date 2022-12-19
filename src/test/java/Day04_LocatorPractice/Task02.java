90618859d1cddd42742619c215423f5cd14b9597
ademalabas@gmail.com <ademalabas@gmail.com>
2022-12-19 21:00:24 +0300

    19.12.22

--- src/test/java/Day04_LocatorPractice/Task02.java
+++ src/test/java/Day04_LocatorPractice/Task02.java
@@ -1,7 +1,15 @@
 package Day04_LocatorPractice;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 
 public class Task02 {
 //Navigate to https://testpages.herokuapp.com/styled/index.html
@@ -13,12 +21,53 @@ public class Task02 {
 // Print the result.
 
 
-WebDriver driver;
+    WebDriver driver;
 
-@Before
+    @Before
+
     public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().window().maximize();
 
-}
+    }
+
+    @After
+    public void tearDown() {
+        // driver.quit();
+
+    }
+//Navigate to https://testpages.herokuapp.com/styled/index.html
+
+    @Test
+    public void idLocator(){
+        driver.get("https://testpages.herokuapp.com");
+
+        // Click on Calculate under Micro Apps.
+        driver.findElement(By.xpath("//a[@id='calculatetest']")).click();
+
+        // Type any number in the first input.
+
+        driver.findElement(By.xpath("//input[@id='number1']")).sendKeys("5240");
+
+        // Type any number in the second input
+        driver.findElement(By.xpath("//input[@id='number2']")).sendKeys("964");
+
+        // Click on Calculate.
+
+        driver.findElement(By.xpath("//input[@id='calculate']")).click();
+
+        // Get the result.
+
+        WebElement sonuc =driver.findElement(By.xpath("//span[@id='answer']"));
+
+
+// Print the result.
+
+        System.out.println(sonuc.getText());
+
+    }
 
 
 
---
+++ src/test/java/Day05_LocatorPractice/C01_RelativeLocator.java
@@ -0,0 +1,60 @@
+package Day05_LocatorPractice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+
+import java.time.Duration;
+
+public class C01_RelativeLocator {
+// Go to http://www.bestbuy.com
+// Verify that the page title contains "Best" Also, using Relative Locator.
+// LogoTest  Verify if the BestBuy logo is displayed.
+// mexicoLinkTest  Verify if the link is displayed.
+
+
+
+    WebDriver driver;
+
+
+    @Before
+    public void setUp() {
+        // Driver ile ilgili her  turlu initial baslangic isimleri burada yapilir
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+
+    }
+
+
+    @After
+    public void tearDown() {
+        // test sonrasinda driver kapatacak (varsa raporlari dosyalamak) icin kullanilir
+        driver.quit();
+    }
+    @Test
+    public void relativeLocator(){
+        driver.get("http://www.bestbuy.com");
+       // String title = driver.getTitle();
+       // Assert.assertTrue(title.contains("Best"));
+
+        Assert.assertTrue("Title Best Kelimesini icermiyor ",driver.getTitle().contains("Best") );
+
+        //logo
+        RelativeLocator.with(By.tagName())
+
+
+
+
+
+
+
+    }
