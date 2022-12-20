c7dbb6e50e3de53545707271b3aa798944f408f2
ademalabas@gmail.com <ademalabas@gmail.com>
2022-12-20 22:17:45 +0300

    20.12.22

--- src/test/java/Day06_Dropdowm_SeleniumWaits/C02_DropDawn_Select.java
+++ src/test/java/Day06_Dropdowm_SeleniumWaits/C02_DropDawn_Select.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 import java.time.Duration;
+import java.util.List;
 
 public class C02_DropDawn_Select {
 
@@ -51,9 +52,75 @@ public class C02_DropDawn_Select {
 
 
     @Test
-    public void selectByValue() {}
+    public void selectByValue() {
+        driver.get("https://demo.guru99.com/test/newtours/register.php");
+        //Dropdown locate edildi
+        WebElement dropdown =driver.findElement(By.name("country"));
+
+        //Locate edilen dropdown select objesine donusturuldu
+        Select select = new Select(dropdown);
+        select.selectByValue("TURKEY");
+
+
+    }
 
     @Test
-    public void selectByVisibleText() {}
+    public void selectByVisibleText() {
+        driver.get("https://demo.guru99.com/test/newtours/register.php");
+        //Dropdown locate edildi
+        WebElement dropdown =driver.findElement(By.name("country"));
+
+        //Locate edilen dropdown select objesine donusturuldu
+        Select select = new Select(dropdown);
+        select.selectByVisibleText("NEPAL");
+        System.out.println("select.isMultiple() = " + select.isMultiple());
+
+    }
+
+
+    @Test
+    public void multipleSelect() {
+        driver.get("https://output.jsbin.com/osebed/2");
+        Select select = new Select(driver.findElement(By.id("fruits")));
+        System.out.println("select.isMultiple() = " + select.isMultiple());
+        select.selectByValue("apple");
+        select.deselectByIndex(3);//grape
+
+    }
+
+@Test
+    public void getOptions() {
+        driver.get("https://demo.guru99.com/test/newtours/register.php");
+        //Dropdown locate edildi
+        Select select= new Select( driver.findElement(By.name("country")));
+    // getOptions() metodu select elementinin tum seceneklerini (optionlarini) bir liste icerisinde bize verir
+        List<WebElement> options = select.getOptions();
+
+        for (WebElement option:options){
+            System.out.println("option.getText() = " + option.getText());
+        }
+        // options.stream().forEach(x-> System.out.println(x.getText()));
+
+
+    }
+
+    @Test
+    public void getFirstSelectedOption(){
+        driver.get("https://output.jsbin.com/osebed/2");
+      Select select = new Select(driver.findElement(By.id("fruits")));
+
+      select.selectByIndex(3);//grape
+      select.selectByIndex(2);//orange
+
+       WebElement selectedoptions= select.getFirstSelectedOption();
+        System.out.println("selectedoptions.getText() = " + selectedoptions.getText());
+        //select.getFirstSelectedOption().getText()
+
+        //getFirstSelectedOption() metodu secili olan
+        // ilk option elementini bize verir. (Sectigimiz ilk option degil !!!)
+
+
+    }
+
 
 }
