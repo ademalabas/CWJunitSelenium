251961f7c6334f2b4587e19155c93dcd8af2128a
ademalabas@gmail.com <ademalabas@gmail.com>
2022-12-22 00:11:32 +0300

    22.12.22

--- src/test/java/Day07_Iframe_WindoHandles/C03_WindowHandles.java
+++ src/test/java/Day07_Iframe_WindoHandles/C03_WindowHandles.java
@@ -55,3 +55,47 @@ Set<String> windows = driver.getWindowHandles();
 
 
 }
+/*
+
+ @Test
+    public void windowHandles(){
+​
+        driver.get("https://www.toolsqa.com/selenium-webdriver/window-handle-in-selenium/");
+​
+​
+        //Daha sonra kullanabilmek adina ana sayfa id mizi sakliyoruz
+        String homePageId = driver.getWindowHandle();
+​
+​
+        // Yeni sekmeler olusturmak adina target = _blank olan linklerden 2 tanesine tikliyoruz
+        List<WebElement> links = driver.findElements(By.xpath("//a[@target='_blank']"));
+​
+        links.get(0).click();
+        links.get(1).click();
+​
+​
+        //Iterate etmek icin (gezinmek icin) tum tab ve window id lerini bir set icinde sakliyoruz
+        Set<String> windows = driver.getWindowHandles();
+​
+        //Gezinme islemi icin iterator objesini olusturuyoruz
+        Iterator<String> iterator = windows.iterator();
+​
+        //Gezinme islemi
+        while (iterator.hasNext()){
+​
+            //Her bir gezinilen id kullanilarak o anki sekmeye switch ediyoruz
+            driver.switchTo().window(iterator.next());
+​
+            //Switch ettigimiz sayfanin ana sayfa olup olamdigini kontrol ediyoruz. Ana sayfa ise sekmeyi kapatmiyoruz degilse kapatiyoruz
+            if(driver.getWindowHandle().equals(homePageId)){
+​
+                //driver.close u es geciyoruz
+                continue;
+            }
+​
+            //Sekmeyi kapatiyoruz
+            driver.close();
+​
+            }
+        }
+ */
