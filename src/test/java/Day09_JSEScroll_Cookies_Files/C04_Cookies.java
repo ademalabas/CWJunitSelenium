3d8645a841c8534aad7bd05c962bbe1f3466b550
ademalabas@gmail.com <ademalabas@gmail.com>
2022-12-23 22:22:45 +0300

    23.12.22

---
+++ src/test/java/Day09_JSEScroll_Cookies_Files/C04_Cookies.java
@@ -0,0 +1,33 @@
+package Day09_JSEScroll_Cookies_Files;
+
+import org.junit.Test;
+import utilities.BaseTest;
+
+public class C04_Cookies extends BaseTest {
+
+/*
+Go to URL: https://kitchen.applitools.com/ingredients/cookie
+Get Cookie.
+Find the total number of cookies.
+Print all the cookies.
+Add Cookie.
+Edit Cookie.
+Delete Cookie.
+Delete All Cookies.
+ */
+
+    @Test
+    public void testCookies(){
+        driver.get("https://kitchen.applitools.com/ingredients/cookie");
+
+
+
+    }
+
+
+
+
+
+
+
+}
