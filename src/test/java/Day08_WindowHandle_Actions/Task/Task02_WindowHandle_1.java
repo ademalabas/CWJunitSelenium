93346b90a63b2e85033aa0e611bfbd385ff1c974
ademalabas@gmail.com <ademalabas@gmail.com>
2022-12-23 14:41:26 +0300

    23.12.22

---
+++ src/test/java/Day08_WindowHandle_Actions/Task/Task02_WindowHandle_1.java
@@ -0,0 +1,29 @@
+package Day08_WindowHandle_Actions.Task;
+
+import org.junit.Test;
+import utilities.BaseTest;
+
+public class Task02_WindowHandle_1 extends BaseTest {
+
+
+
+    @Test
+    public void windowHandle1 (){
+       // Go to URL: https://testproject.io/
+
+
+       // Selenium’s 4 newWindow() method to open a new Window for TestProject’s Blog page.
+
+
+        //   https://blog.testproject.io/
+
+
+
+
+    }
+
+
+
+
+
+}
---
+++ src/test/java/Day08_WindowHandle_Actions/Task/Task03_WindowHandle_2.java
@@ -0,0 +1,34 @@
+package Day08_WindowHandle_Actions.Task;
+
+import org.junit.Test;
+import utilities.BaseTest;
+
+public class Task03_WindowHandle_2 extends BaseTest {
+
+
+
+    @Test
+    public void windowHandle2 (){
+        //Go to URL: https://demoqa.com/browser-windows
+
+
+       // Click on the windows - "WindowButton"
+
+
+       // Click on all the child windows.
+
+
+       // Print the text present on all the child windows in the console.
+
+
+       // Print the heading of the parent window in the console.
+
+
+
+    }
+
+
+
+
+
+}
