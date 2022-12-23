1e2c27ba0ad4e8a29fa746c75ee29904ef71bc32
ademalabas@gmail.com <ademalabas@gmail.com>
2022-12-23 14:37:11 +0300

    23.12.22

---
+++ src/test/java/Day08_WindowHandle_Actions/Task/Task01_Iframe.java
@@ -0,0 +1,24 @@
+package Day08_WindowHandle_Actions.Task;
+
+import org.junit.Test;
+import utilities.BaseTest;
+
+public class Task01_Iframe extends BaseTest {
+
+
+    @Test
+
+    public void Iframe() {
+
+        // Go to URL: http://demo.guru99.com/test/guru99home/
+        driver.get("http://demo.guru99.com/test/guru99home/");
+
+        // Find the number of iframes on the page.
+        //Link to the fourth iframe (JMeter Made Easy) (https://www.guru99.com/live selenium project.html) click here.
+        // Exit the iframe and return to the main page.
+
+    }
+
+
+
+}
