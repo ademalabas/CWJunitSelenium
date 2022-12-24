badbb441ef509811e5352fa64c6492d04b74af9a
ademalabas@gmail.com <ademalabas@gmail.com>
2022-12-24 21:13:19 +0300

    24.12.22

---
+++ src/test/java/CodeChallange/Task_01.java
@@ -0,0 +1,31 @@
+package CodeChallange;
+
+import org.junit.Test;
+import utilities.BaseTest;
+
+public class Task_01 extends BaseTest {
+
+    @Test
+    public void test() {
+
+/*
+    1. "https://facebook.com" adresine gidin.
+    2. "Create New Account" butonuna basın.
+    3. "firstName" giriş kutusuna bir isim yazın.
+    4. "surname" giriş kutusuna bir soyisim yazın.
+    5. "email" giriş kutusuna bir email yazın.
+    6. "email" onay kutusuna emaili tekrar yazın.
+    7. Bir şifre girin.
+    8. Tarih için gün seçin.
+    9. Tarih için ay seçin.
+    10. Tarih için yıl seçin.
+    11. Cinsiyeti seçin.
+    12. İşaretlediğiniz cinsiyetin seçili, diğer cinsiyet kutularının seçili olmadığını doğrulayın.
+    13. Sayfayı kapatın.
+*/
+
+    }
+
+
+
+}
