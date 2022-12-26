c2448fa23395c439f4bd2b1b524428689a6e5dfa
ademalabas@gmail.com <ademalabas@gmail.com>
2022-12-26 21:54:02 +0300

    26.12.22

--- src/test/java/Day10/C09_FileDownload.java
+++ src/test/java/Day10/C09_FileDownload.java
@@ -1,10 +1,14 @@
 package Day10;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import utilities.BaseTest;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class C09_FileDownload extends BaseTest {
 
@@ -15,23 +19,46 @@ public class C09_FileDownload extends BaseTest {
     }
 
     @Test
-    public void test() {
+    public void test() throws InterruptedException {
 
         //Go to URL: https://opensource-demo.orangehrmlive.com/
         //Login page valid credentials.
        // Download sample CSV file.
        // Verify if the file downloaded successfully.
         driver.get("https://opensource-demo.orangehrmlive.com/");
-        WebElement userName = driver.findElement(By.name("username"));
-        userName.sendKeys("Admin");
+
+        // Sisteme giris yapiyoruz
+        WebElement username = driver.findElement(By.name("username"));
+        username.sendKeys("Admin");
+
         WebElement password = driver.findElement(By.name("password"));
-        password.sendKeys("admin123" + Keys.ENTER);
+        password.sendKeys("admin123");
 
         WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
         loginButton.click();
 
+        // Indirilecek dosyaya gitmek icin gerekli adimlar
+        WebElement PIM = driver.findElement(By.xpath("(//a[@class='oxd-main-menu-item'])[2]"));
+        PIM.click();
 
+        WebElement configurationTab = driver.findElement(By.xpath("//span[text()='Configuration ']"));
+        configurationTab.click();
 
+        WebElement dataImport = driver.findElement(By.linkText("Data Import"));
+        dataImport.click();
+
+        WebElement downloadLink = driver.findElement(By.className("download-link"));
+        downloadLink.click();
+
+        Thread.sleep(5000); // Ne olur ne olmaz dosyanin inmesi icin bekliyoruz
+
+        // Indirilen dosyamizin path i
+        String downloadPath = "C:\\Users\\hi\\Downloads\\importData.csv";
+
+        // Dosyanin indirildigini kontrol ediyoruz
+        Assert.assertTrue("Indirilen dosya bulunamadi", Files.exists(Paths.get(downloadPath)));
+
+    }
 
 
 
@@ -40,4 +67,4 @@ public class C09_FileDownload extends BaseTest {
 
 
 
-}
+
