d6fb167c093189c025fd1480c854d7181419ec31
ademalabas@gmail.com <ademalabas@gmail.com>
2023-01-03 15:15:14 +0300

    03.01.23

--- src/test/java/Day13_Log4j_SeleniumExceptions_ExtentReport/C02_Log4jDemo.java
+++ src/test/java/Day13_Log4j_SeleniumExceptions_ExtentReport/C02_Log4jDemo.java
@@ -1,15 +1,14 @@
 package Day13_Log4j_SeleniumExceptions_ExtentReport;
 
 
+import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-public class C01_Log4jDemo {
+public class C02_Log4jDemo {
 
     public static void main(String[] args) {
         PropertyConfigurator.configure("log4j.properties");
-        Logger logger = LogManager.getLogger(C01_Log4jDemo.class.getName());
+        Logger logger =Logger.getLogger(C02_Log4jDemo.class.getName());
 
         logger.trace("Trace Message");
         logger.debug("Debug Message");
