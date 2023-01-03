e47ccef8d48591a5e97e65d23971106405fa9e2f
ademalabas@gmail.com <ademalabas@gmail.com>
2023-01-03 14:51:43 +0300

    03.01.23

---
+++ src/test/java/Day13_Log4j_SeleniumExceptions_ExtentReport/C01_Log4jDemo.java
@@ -0,0 +1,21 @@
+package Day13_Log4j_SeleniumExceptions_ExtentReport;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class C01_Log4jDemo {
+
+    public static void main(String[] args) {
+
+        Logger logger = LogManager.getLogger(C01_Log4jDemo.class.getName());
+
+        logger.trace("Trace Message");
+        logger.debug("Debug Message");
+        logger.info("Info Message");
+        logger.warn("Warn Message");
+        logger.error("Error Message");
+        logger.fatal("Fatal Message");
+
+    }
+}
--- src/test/java/utilities/Log4j.java
+++ src/test/java/utilities/Log4j.java
@@ -1,8 +1,51 @@
 package utilities;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Log4j {
 
+    private static Logger log = LogManager.getLogger(Log4j.class.getName());
 
+        
+    public static void startLog (String testClassName){
+        log.info("Test is Starting...");
+    }
+
+        
+    public static void endLog (String testClassName){
+        log.info("Test is Ending...");
+    }
+
+    //Info Level
+    public static void info (String message) {
+        log.info(message);
+    }
+
+    //Warn Level
+    public static void warn (String message) {
+        log.warn(message);
+    }
+    //Trace Level
+    public static void trace (String message) {
+        log.trace(message);
+    }
+
+    //Error Level
+    public static void error (String message) {
+        log.error(message);
+    }
+
+    //Fatal Level
+    public static void fatal (String message) {
+        log.fatal(message);
+    }
+
+    //Debug Level
+    public static void debug (String message) {
+        log.debug(message);
+    }
 
 
 }
